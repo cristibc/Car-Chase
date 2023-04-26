655a67060444dcdbd033a577695a2ef90ed37790
cristibc <90398658+cristibc@users.noreply.github.com>
2023-04-26 16:26:58 +0300

    implemented coins

--- main.cpp
+++ main.cpp
@@ -26,6 +26,15 @@ bool initObstacle1, initObstacle2, initObstacle3;
 double iObstacol1, iObstacol2, iObstacol3;
 bool collisionCheck = false;
 double score = 0;
+double speed = 0;
+float genR, genG, genB;
+float genR1, genG1, genB1;
+float genR2, genG2, genB2;
+float genR3, genG3, genB3;
+int chance = 50;
+double xCoin = 0;
+double yCoin = 320;
+bool displayCoin = false;
 
 
 void delay(unsigned int milisecunde)
@@ -41,7 +50,12 @@ void increaseScore(int value) {
 	glutTimerFunc(100, increaseScore, 0);
 }
 
-
+void increaseSpeed(int value) {
+	if (collisionCheck != true) {
+		speed += 1;
+	}
+	glutTimerFunc(100, increaseSpeed, 0);
+}
 
 class scrPt
 {
@@ -77,8 +91,57 @@ void displayText(int x, int y, float r, float g, float b, const char* string)
 	}
 }
 
+void generateLucky() {
+	int isLucky;
+	xCoin = 0;
+	yCoin = 320;
+	displayCoin = false;
+	isLucky = rand() % chance;
+	if (isLucky == 0) {
+		cout << "Lucky one!" << endl;
+		chance = 50;
+		displayCoin = true;
+	}
+	else {
+		cout << "You're not lucky! chance: 1/" << chance << endl;
+		chance = chance / 2;
+	}
+}
 
+void generateColor(void) {
 
+	int pickPrimary = rand() % 6;
+	if (pickPrimary == 0) {
+		genR = 1;
+		genG = (float)rand() / (float)RAND_MAX;
+		genB = 0;
+	}
+	else if (pickPrimary == 1) {
+		genR = (float)rand() / (float)RAND_MAX;
+		genG = 1;
+		genB = 0;
+	}
+	else if (pickPrimary == 2) {
+		genR = 0;
+		genG = 1;
+		genB = (float)rand() / (float)RAND_MAX;
+	}
+	else if (pickPrimary == 3) {
+		genR = 0;
+		genG = (float)rand() / (float)RAND_MAX;
+		genB = 1;
+	}
+	else if (pickPrimary == 4) {
+		genR = (float)rand() / (float)RAND_MAX;
+		genG = 0;
+		genB = 1;
+	}
+	else if (pickPrimary == 5) {
+		genR = 1;
+		genG = 0;
+		genB = (float)rand() / (float)RAND_MAX;
+	}
+}
 
 void Obstacole(int value) {
 	// Initializam indicele pentru translatie
@@ -93,6 +156,8 @@ void Obstacole(int value) {
 
 	// Generam un set de obstacole pozitionate random, intre 1 si 2 obstacole
 	if (collisionCheck != true) {
+		generateLucky();
+
 		srand(time(NULL));
 		cout << "Called function" << endl;
 		obstacol1 = rand() % 2;
@@ -111,86 +176,116 @@ void Obstacole(int value) {
 
 		if (obstacol1 == 1) {
 			initObstacle1 = true;
+			generateColor();
+			genR1 = genR;
+			genG1 = genG;
+			genB1 = genB;
 		}
 
 		if (obstacol2 == 1) {
 			initObstacle2 = true;
+			generateColor();
+			genR2 = genR;
+			genG2 = genG;
+			genB2 = genB;
 		}
 
 		if (obstacol3 == 1) {
 			initObstacle3 = true;
+			generateColor();
+			genR3 = genR;
+			genG3 = genG;
+			genB3 = genB;
 		}
 	}
+
 	// Timer pentru loop-ul obstacolelor
 	glutTimerFunc(4000, Obstacole, 0);
 
 }
 
-void Collision(double x1, double y1, double x2, double y2, double xWidth, double xHeight, double yWidth, double yHeight) {
+bool Collision(double x1, double y1, double x2, double y2, double xWidth, double xHeight, double yWidth, double yHeight) {
 	//double x1, y1, x2, y2, width, height;
 	if (x1 < x2 + yWidth && x1 + xWidth > x2 && y1 < y2 + yHeight && y1 + xHeight > y2)
 	{
-		cout << "Collision detected\n";
-		collisionCheck = true;
+		//collisionCheck = true;
+		return 1;
 	}
-
+	return 0;
 }
 
-void buildCar(int x, int y) {
+void buildCar(int x, int y, float R, float G, float B) {
 	//Model Initial
 	//glRectf(-160, 270, -100, 360);
-	float R = 0;
-	float G = 0;
-	float B = 0;
+	if (initObstacle1 == 0 && initObstacle2 == 0 && initObstacle3 == 0) {
+		generateColor();
+	}
 
-	int pickPrimary = rand() % 6;
-	if (pickPrimary == 0) {
-		R = 1;
-		G = (float)rand() / (float)RAND_MAX;
-		B = 0;
-	}
-	else if (pickPrimary == 1) {
-		R = (float)rand() / (float)RAND_MAX;
-		G = 1;
-		B = 0;
-	}
-	else if (pickPrimary == 2) {
-		R = 0;
-		G = 1;
-		B = (float)rand() / (float)RAND_MAX;
-	}
-	else if (pickPrimary == 3) {
-		R = 0;
-		G = (float)rand() / (float)RAND_MAX;
-		B = 1;
-	}
-	else if (pickPrimary == 4) {
-		R = (float)rand() / (float)RAND_MAX;
-		G = 0;
-		B = 1;
-	}
-	else if (pickPrimary == 5) {
-		R = 1;
-		G = 0;
-		B = (float)rand() / (float)RAND_MAX;
-	}
 	//Sasiu
 	glColor3f(R, G, B);
-	glRectf(x, y, x+60, y+25);
-	glColor3f(R-0.3, G-0.3, B-0.3);
-	glRectf(x, y+25, x+60, y+65);
+	glRectf(x, y, x + 60, y + 25);
+	glColor3f(R - 0.3, G - 0.3, B - 0.3);
+	glRectf(x, y + 25, x + 60, y + 65);
 	glColor3f(R, G, B);
-	glRectf(x, y+65, x+60, y+90);
+	glRectf(x, y + 65, x + 60, y + 90);
 	// stopuri
 	glColor3f(1, 0, 0);
-	glRectf(x+5, y-5, x+15, y);
+	glRectf(x + 5, y - 5, x + 15, y);
 	glColor3f(1, 0, 0);
-	glRectf(x+45, y-5, x+55, y);
+	glRectf(x + 45, y - 5, x + 55, y);
 	// faruri fata
 	glColor3f(1, 1, 0);
-	glRectf(x+5, y+90, x+15, y+95);
+	glRectf(x + 5, y + 90, x + 15, y + 95);
 	glColor3f(1, 1, 0);
-	glRectf(x+45, y+90, x+55, y+95);
+	glRectf(x + 45, y + 90, x + 55, y + 95);
+}
+
+void drawCoin() {
+	glPushMatrix();
+	glScalef(40, 40, 0);
+	glColor3f(0.855, 0.647, 0.125);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(0, 0); // Center of the circle
+	for (int i = 0; i <= 360; i += 5) {
+		float radian = i * (3.14159 / 180.0);
+		float x = 0.5 * cos(radian);
+		float y = 0.5 * sin(radian);
+		glVertex2f(x, y);
+	}
+	glEnd();
+
+	// Draw the inner circle
+	glColor3f(1.0, 0.937, 0.537);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(0, 0); // Center of the circle
+	for (int i = 0; i <= 360; i += 5) {
+		float radian = i * (3.14159 / 180.0);
+		float x = 0.4 * cos(radian);
+		float y = 0.4 * sin(radian);
+		glVertex2f(x, y);
+	}
+	glEnd();
+
+	// Draw the star in the middle
+	glLineWidth(2.0); // Set line width to 2.0
+	glColor3f(0.855, 0.647, 0.125);
+	glBegin(GL_TRIANGLES);
+	float radius = 0.35; // Updated radius of the star
+	for (int i = 0; i < 5; i++) {
+		float angle1 = i * 72 * (3.14159 / 180.0);
+		float angle2 = (i + 2) % 5 * 72 * (3.14159 / 180.0);
+		float x1 = radius * cos(angle1);
+		float y1 = radius * sin(angle1);
+		float x2 = 0.5 * radius * cos(angle2); // Adjusted x-coordinate for chubbier appearance
+		float y2 = 0.5 * radius * sin(angle2); // Adjusted y-coordinate for chubbier appearance
+
+		glVertex2f(x1, y1);
+		glVertex2f(0, 0);
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+	glLineWidth(1.0); // Reset line width to default
+	glPopMatrix();
 }
 
 void miscareGirofar(void) {
@@ -205,36 +300,67 @@ void miscareGirofar(void) {
 
 	// Pentru input-ul left key - right key
 	if (leftUpPressed && i > -190) {
-		i -= 1.3 + score / 1600;
+		i -= 1.3 + speed / 1600;
 	}
 
 	if (rightUpPressed && i < 90) {
-		i += 1.3 + score / 1600;
+		i += 1.3 + speed / 1600;
 	}
 
 	// Indice pentru axul drumului, indica viteza scenei
-	iViteza += 1;
+	iViteza += 1 + speed / 400;
 	if (iViteza > 519) {
-		iViteza = 260 + score / 400;
+		iViteza = 260;
 	}
 
 	// Indice pentru translatia obstacolelor
 	if (initObstacle1 == 1 && iObstacol1 > -650) {
-		iObstacol1 -= 1 + score / 400;
+		iObstacol1 -= 1 + speed / 400;
 	}
 
 	if (initObstacle2 == 1 && iObstacol2 > -650) {
-		iObstacol2 -= 1 + score / 400;
+		iObstacol2 -= 1 + speed / 400;
 	}
 
 	if (initObstacle3 == 1 && iObstacol3 > -650) {
-		iObstacol3 -= 1 + score / 400;
+		iObstacol3 -= 1 + speed / 400;
 	}
 
-	// Collision check
-	Collision(-160, iObstacol1 + 360, i + 20, -90, 60, 90, 60, 90);
-	Collision(-35, iObstacol2 + 360, i + 20, -90, 60, 90, 60, 90);
-	Collision(95, iObstacol3 + 360, i + 20, -90, 60, 90, 60, 90);
+	if (initObstacle1 == 0 && displayCoin == true) {
+		xCoin = -150;
+		yCoin -= 1 + speed / 400;
+	}
+	else if (initObstacle2 == 0 && displayCoin == true) {
+		xCoin = 0;
+		yCoin -= 1 + speed / 400;
+	}
+	else if (initObstacle3 == 0 && displayCoin == true) {
+		xCoin = 135;
+		yCoin -= 1 + speed / 400;
+	}
+
+	// Collision check for obstacles
+	if (Collision(-150, iObstacol1 + 360, i + 20, -90, 60, 90, 60, 90) == 1) {
+		collisionCheck = true;
+	}
+
+	if (Collision(-35, iObstacol2 + 360, i + 20, -90, 60, 90, 60, 90) == 1) {
+		collisionCheck = true;
+	}
+
+	if (Collision(95, iObstacol3 + 360, i + 20, -90, 60, 90, 60, 90) == 1) {
+		collisionCheck = true;
+	}
+
+	// Collision check for coins
+	if (Collision(xCoin - 15, yCoin + 65, i + 20, -90, 40, 40, 60, 90) == 1) {
+		cout << "Touched coin!";
+		xCoin = 0;
+		yCoin = -300;
+		score += 100;
+		sndPlaySound(TEXT("coin.wav"), SND_ASYNC);
+
+	}
 
 	//TODO add power-ups
 	glutPostRedisplay();
@@ -251,9 +377,7 @@ static void init(void)
 	glClearColor(0, 0.839, 0.082, 1);
 	gluOrtho2D(-100, 100, -100, 100);
 
-
 	traficID = glGenLists(1);
-
 
 	for (aux = 0; aux < 20; aux++)
 	{
@@ -270,8 +394,7 @@ static void init(void)
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// Muzica fundal
-	sndPlaySound(TEXT("nightrider.wav"), SND_ASYNC);
-	//playSound("nightrider.wav")
+	//sndPlaySound(TEXT("nightrider.wav"), SND_ASYNC);
 
 }
 
@@ -304,6 +427,7 @@ void keyPressed(int key, int x, int y)
 		glClearColor(0, 0.839, 0.082, 1);
 		collisionCheck = false;
 		score = 0;
+		speed = 0;
 		break;
 	default:
 		break;
@@ -378,15 +502,15 @@ void desenDrum(void)
 		//glRectf(65, -90, 75, -85);
 
 
-		//glColor3f(1, 1, 0);
-		//glBegin(GL_TRIANGLES);
-		//glVertex2f(30, -85);
-		//glVertex2f(0, 0);
-		//glVertex2f(60, 0);
+		glColor4f(1, 1, 0, 0.05);
+		glBegin(GL_TRIANGLES);
+		glVertex2f(30, -85);
+		glVertex2f(0, 0);
+		glVertex2f(60, 0);
 
-		//glVertex2f(40, 0);
-		//glVertex2f(100, 0);
-		//glVertex2f(70, -85);
+		glVertex2f(40, 0);
+		glVertex2f(100, 0);
+		glVertex2f(70, -85);
 
 		glEnd();
 
@@ -425,8 +549,7 @@ void desenDrum(void)
 		glTranslated(0, iObstacol1, 0);
 		//glColor3f(0, 0, 0);
 		//glRectf(-160, 270, -100, 360);
-		srand(time(NULL));
-		buildCar(-160, 270);
+		buildCar(-160, 270, genR1, genG1, genB1);
 
 		glPopMatrix();
 
@@ -435,9 +558,7 @@ void desenDrum(void)
 		glTranslated(0, iObstacol2, 0);
 		//glColor3f(0, 0, 0);
 		//glRectf(-35, 270, 25, 360);
-
-		srand(time(NULL));
-		buildCar(-35, 270);
+		buildCar(-35, 270, genR2, genG2, genB2);
 
 		glPopMatrix();
 
@@ -447,13 +568,21 @@ void desenDrum(void)
 		//glColor3f(0, 0, 0);
 		//glRectf(95, 270, 155, 360);
 
-		srand(time(NULL));
-		buildCar(95, 270);
+		buildCar(95, 270, genR3, genG3, genB3);
 		glPopMatrix();
 
+		// Coin
+
+		glPushMatrix();
+		glTranslated(xCoin, yCoin, 0);
+		drawCoin();
+		glPopMatrix();
+
+
+
+		// Layer transparent overlay blue
 		glColor4f(0.012, 0.0, 0.529, 0.25);
 		glRectf(-1000., -1000., 1000, 1000.);
-
 
 		// incrementare pentru girofar si alte translatii
 		glutIdleFunc(miscareGirofar);
@@ -519,6 +648,8 @@ void main(int argc, char** argv)
 
 	glutTimerFunc(5000, Obstacole, 0);
 	glutTimerFunc(100, increaseScore, 0);
+	glutTimerFunc(100, increaseSpeed, 0);
+
 
 	glutSpecialFunc(keyPressed);
 	glutSpecialUpFunc(keyUp);
